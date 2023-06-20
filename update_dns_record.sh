6f6a4da33bfc7557224318a911aa2d9ffe5a9643
Dries Verachtert <dries.verachtert@epfl.ch>
2023-06-20 13:30:32 +0000

    BBPP154-75 Automatically update dns record of sbo-poc-pcluster.shapes-registry.org once the cluster deployment is finished.

---
+++ update_dns_record.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# To be used within that pcluster image: expects pcluster to work, with aws credentials configured
+
+# Make sure the cluster is ready
+echo "Checking if the cluster is ready"
+cluster_status=$(pcluster describe-cluster --region us-east-1 -n hpc-cluster | jq -r .clusterStatus)
+while [ "$cluster_status" != "CREATE_COMPLETE" ]
+do 
+    echo "Cluster is not yet ready, status is ${cluster_status}"
+    sleep 5s
+    cluster_status=$(pcluster describe-cluster --region us-east-1 -n hpc-cluster | jq -r .clusterStatus)
+done
+echo "The cluster is ready"
+
+# Install the aws cli
+yum install -y yq unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Get the ID of the dns zone
+zone_id=$(aws route53 list-hosted-zones-by-name | jq -r '.HostedZones[] | select(.Name=="shapes-registry.org.") | .Id')
+echo "Zone id: ${zone_id}"
+
+# Get the ip address of the head node
+ip_address=$(pcluster describe-cluster --region us-east-1 -n hpc-cluster | jq -r .headNode.privateIpAddress)
+echo "Ip address: ${ip_address}"
+
+# name of the dns record
+record_name="sbo-poc-pcluster.shapes-registry.org"
+echo "Record name: ${record_name}"
+
+# update the record
+aws route53 change-resource-record-sets --hosted-zone-id /hostedzone/Z08554442LEJ4EBB4CAIQ --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$record_name'","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$ip_address'"}]}}]}'
+
+# an update can take up to a minute it seems
+sleep 60s
+
+# print the current value in the DNS zone:
+echo "Check which ip address is now used in the zone:"
+aws route53 list-resource-record-sets --hosted-zone-id $zone_id | jq -r '.ResourceRecordSets[] | select(.Name=="sbo-poc-pcluster.shapes-registry.org.") | .ResourceRecords[0].Value'
