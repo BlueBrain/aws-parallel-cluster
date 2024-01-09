f73dbc2e1ead23783ff37bfcb750fdc25f001d54
Sergio <sergio.rivasgomez@epfl.ch>
2024-01-09 13:37:49 +0100

    [NOTICKET] Update CLI packages, including AWS CLI

--- scripts/s3/install_packages.sh
+++ scripts/s3/install_packages.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-yum install -y tmux vim
+# Install common packages
+yum install -y tmux vim htop clustershell
+
+# Update the AWS CLI version
+yum remove -y awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+unzip /tmp/awscliv2.zip -d /tmp/awscliv2
+/tmp/awscliv2/aws/install
+rm -rf /tmp/awscliv2*
