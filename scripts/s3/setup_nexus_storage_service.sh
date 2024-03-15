1fa1b9ace22eaeaec8726c509946d53823bf2790
Omar Awile <omar.awile@epfl.ch>
2024-03-15 08:56:07 +0000

    [BBPP154-198] Fix service dependency and wait for the SIF file to be available

--- scripts/s3/setup_nexus_storage_service.sh
+++ scripts/s3/setup_nexus_storage_service.sh
@@ -7,7 +7,8 @@ UNITFILE=/etc/systemd/system/nexus-storage.service
 cat <<\EOF >$UNITFILE
 [Unit]
 Description=Nexus Storage
-After=network.target
+Requires=sbo-data.mount
+After=sbo-data.mount
 
 [Service]
 Type=simple
@@ -19,7 +20,7 @@ WorkingDirectory=/root
 Environment="NEXUS_SIF=/sbo/data/containers/nexus-storage.sif"
 Environment="NEXUS_LATEST_SIF=/sbo/data/containers/nexus-storage-latest.sif"
 Environment="SINGULARITYENV_STORAGE_CONFIG_FILE=/sbo/data/project/storage.conf"
-ExecStartPre=/usr/bin/bash -c 'if [ -f ${NEXUS_LATEST_SIF} ]; then mv ${NEXUS_LATEST_SIF} ${NEXUS_SIF}; fi'
+ExecStartPre=/usr/bin/bash -c 'while [ ! -f ${NEXUS_SIF} ]; do sleep 10; done; if [ -f ${NEXUS_LATEST_SIF} ]; then mv ${NEXUS_LATEST_SIF} ${NEXUS_SIF}; fi'
 ExecStart=/usr/bin/singularity run --bind /sbo/data ${NEXUS_SIF} \
   -Dapp.instance.interface="0.0.0.0" \
   -Dakka.http.server.parsing.max-content-length="100g" \
