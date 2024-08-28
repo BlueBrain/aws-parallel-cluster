e83eff5fece1231cecbebbcec651c9a2c943cc28
Sergio <sergio.rivasgomez@epfl.ch>
2024-08-28 17:02:40 +0000

    [NOTICKET] Disable containers S3 sync from S3 + Change login node to t3.micro

--- scripts/s3/setup_lustrefsxonefs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LUSTREFSX_DIR=${1:-"/sbo/data"}
-
-# Create the containers and project directories on EFS
-mkdir -p ${LUSTREFSX_DIR}/containers ${LUSTREFSX_DIR}/project
-
-# Sync the containers directory to the location on EFS
-aws s3 sync s3://sboinfrastructureassets/containers/ ${LUSTREFSX_DIR}/containers/
