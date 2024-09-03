6994d866a1f8cf6be0408c5c525e939d5964d29b
Sergio Rivas Gomez <sergio.rivasgomez@epfl.ch>
2024-09-03 09:12:22 +0000

    [BBPP154-308] Upgrade to ParallelCluster 3.10.1 + Amazon Linux 2023

---
+++ scripts/s3/ami/install_packages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install the tools that we will need in the ParallelCluster
+dnf search htop && dnf install -y htop
+pip install ClusterShell
+
+# Install the latest AWS CLI version
+dnf remove -y awscli
+wget -O "/tmp/awscliv2.zip" "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+unzip /tmp/awscliv2.zip -d /tmp/awscliv2
+/tmp/awscliv2/aws/install
+rm -rf /tmp/awscliv2*
+
+# Install Singularity and its dependencies
+SINGULARITY_VERSION="4.1.5"
+SINGULARITY_PACKAGE="singularity-ce-${SINGULARITY_VERSION}.tar.gz"
+wget --no-verbose "https://github.com/sylabs/singularity/releases/download/v${SINGULARITY_VERSION}/${SINGULARITY_PACKAGE}"
+dnf install -y rpm-build autoconf automake fuse3-devel glib2-devel golang libseccomp-devel libtool squashfs-tools zlib-devel
+rpmbuild -tb ${SINGULARITY_PACKAGE}
+dnf install -y ./rpmbuild/RPMS/x86_64/singularity-ce-${SINGULARITY_VERSION}-1.amzn2023.x86_64.rpm
+rm -f ${SINGULARITY_PACKAGE}
