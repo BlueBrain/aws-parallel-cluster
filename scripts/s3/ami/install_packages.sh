3b58035a14df7814660cb947ef53b1b85e778a5b
Sergio Rivas Gomez <sergio.rivasgomez@epfl.ch>
2024-09-06 14:06:05 +0000

    [BBPP154-308] Fix Singularity installation issue in custom AMI

--- scripts/s3/ami/install_packages.sh
+++ scripts/s3/ami/install_packages.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
+
+SINGULARITY_VERSION=${1:-"4.2.0"}
+CRUN_VERSION=${2:-"1.16.1"}
+
+# Setup a temporary directory
+tmpdir=$(mktemp -d)
 
 # Install the tools that we will need in the ParallelCluster
 dnf search htop && dnf install -y htop
@@ -8,16 +14,63 @@ pip install ClusterShell
 
 # Install the latest AWS CLI version
 dnf remove -y awscli
-wget -O "/tmp/awscliv2.zip" "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
-unzip /tmp/awscliv2.zip -d /tmp/awscliv2
-/tmp/awscliv2/aws/install
-rm -rf /tmp/awscliv2*
+cd ${tmpdir}
+wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+unzip awscli*.zip
+./aws/install
 
-# Install Singularity and its dependencies
-SINGULARITY_VERSION="4.1.5"
-SINGULARITY_PACKAGE="singularity-ce-${SINGULARITY_VERSION}.tar.gz"
-wget --no-verbose "https://github.com/sylabs/singularity/releases/download/v${SINGULARITY_VERSION}/${SINGULARITY_PACKAGE}"
-dnf install -y rpm-build autoconf automake fuse3-devel glib2-devel golang libseccomp-devel libtool squashfs-tools zlib-devel
-rpmbuild -tb --clean --nodebuginfo ${SINGULARITY_PACKAGE}
-dnf install -y /rpmbuild/RPMS/x86_64/singularity-ce-${SINGULARITY_VERSION}-1.amzn2023.x86_64.rpm
-rm -f ${SINGULARITY_PACKAGE} /rpmbuild
+# Install Go for Singularity
+GO_VERSION="1.22.4"
+cd ${tmpdir}
+wget https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
+tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+export PATH=${PATH}:/usr/local/go/bin
+
+# Install dependencies for Singularity
+dnf groupinstall -y 'Development Tools'
+dependencies=(autoconf \
+              automake \
+              cryptsetup \
+              fuse \
+              fuse3 \
+              fuse3-devel \
+              gcc \
+              git \
+              glib2-devel \
+              glibc-static \
+              go-md2man \
+              libcap-devel \
+              libseccomp-devel \
+              libtool \
+              make \
+              pkg-config \
+              python \
+              python3 \
+              python3-libmount \
+              squashfs-tools \
+              systemd-devel \
+              yajl-devel \
+              zlib-devel)
+for dependency in ${dependencies[@]}; do
+    dnf search ${dependency} && dnf install -y ${dependency}  # Run individually for getting specific errors
+done
+
+# Install crun for Singularity
+cd ${tmpdir}
+git clone --recurse-submodules https://github.com/containers/crun.git --branch ${CRUN_VERSION}
+cd crun
+./autogen.sh
+./configure
+make
+make install
+
+# Install Singularity
+cd ${tmpdir}
+git clone --recurse-submodules https://github.com/sylabs/singularity.git --branch v${SINGULARITY_VERSION}
+cd singularity
+./mconfig
+make -C builddir
+make -C builddir install
+
+# Delete the temporary directory
+rm -rf ${tmpdir}
