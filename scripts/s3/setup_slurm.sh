43071813480476319cc2ffd5a367f8833e437002
Sergio Rivas Gomez <sergio.rivasgomez@epfl.ch>
2024-10-02 07:42:31 +0000

    [BBPP154-308] Fix for automatic '/dev/shm' clean-up + Other changes

--- scripts/s3/setup_slurm.sh
+++ scripts/s3/setup_slurm.sh
@@ -10,6 +10,7 @@ SLURM_CLOUDWATCH_CONFIG=/tmp/slurm_cloudwatch.json
 AWS_CLOUDWATCH_PATH=/opt/aws/amazon-cloudwatch-agent
 AWS_CLOUDWATCH_AGENTCTL=${AWS_CLOUDWATCH_PATH}/bin/amazon-cloudwatch-agent-ctl
 AWS_CLOUDWATCH_CONFIG=${AWS_CLOUDWATCH_PATH}/etc/amazon-cloudwatch-agent.d/file_amazon-cloudwatch-agent.json
+SLURM_LOG_GROUP="$(grep "log_group_name" ${AWS_CLOUDWATCH_CONFIG} | head -n 1 | sed -r "s|^.*: \"(.*)\"$|\1|").slurm-jobs"
 
 # Copy the SLURM script files into the scripts directory and set permissions
 aws s3 cp --recursive s3://sboinfrastructureassets/scripts/slurm/ ${SLURM_SCRIPTS_DIR}
@@ -26,17 +27,13 @@ for config_file in slurm.conf slurmdbd.conf; do
 done
 sed -i "s|/var/log/slurm|${SLURM_LOG_DIR}/slurm|g" ${AWS_CLOUDWATCH_CONFIG}
 
-# Retrieve the CloudWatch log group name from the ParallelCluster and append a suffix
-log_group="$(grep "log_group_name" ${AWS_CLOUDWATCH_CONFIG} | head -n 1 | sed -r "s|^.*: \"(.*)\"$|\1|").slurm-jobs"
-
 # Helper function to define a CloudWatch entry
 function add_cloudwatch_entry {
   echo -ne "{\n\
             \"log_stream_name\": \"${1}\",\n\
-            \"log_group_name\": \"${log_group}\",\n\
+            \"log_group_name\": \"${SLURM_LOG_GROUP}\",\n\
             \"file_path\": \"/sbo/data/scratch/slurm/logs/**-${1}*.*\",\n\
-            \"multi_line_start_pattern\": \"^JobId=[0-9]+.*JobName=[^ ]$\",\n\
-            \"retention_in_days\": ${AWS_CLOUDWATCH_RETENTION}\n\
+            \"multi_line_start_pattern\": \"^JobId=[0-9]+.*JobName=[^ ]$\"\n\
           }"
 }
 
@@ -61,8 +58,9 @@ cat > ${SLURM_CLOUDWATCH_CONFIG} << EOF
 }
 EOF
 
-# Manually create the log group with the tag for billing to ensure that the resource is tracked
-aws logs create-log-group --log-group-name ${log_group} --tags ${TAG}
+# Manually create the log group with the tag for billing and set retention period
+aws logs create-log-group --log-group-name "${SLURM_LOG_GROUP}" --tags "${TAG}"
+aws logs put-retention-policy --log-group-name "${SLURM_LOG_GROUP}" --retention-in-days ${AWS_CLOUDWATCH_RETENTION}
 
 # Append the new configuration file to start monitoring the jobs from SLURM
 ${AWS_CLOUDWATCH_AGENTCTL} -a append-config -m ec2 -s -c file:${SLURM_CLOUDWATCH_CONFIG} && \
